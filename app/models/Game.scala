74e8beb4761919ee56c663bbeab14a6cc70107a3
Leonard David Ricci <ldricci3@gmail.com>
2019-02-19 15:07:27 -0500

    changed game, player from static to dynamic classes

--- app/models/Game.scala
+++ app/models/Game.scala
@@ -1,24 +1,17 @@
 package models
 
 class Game(val names:  Set[String]) {
-  val players = List[Player]
+  require(names.size >= 3 && names.size <= 6, "Must have from 3 to 6 players")
+  val gameMap: GameMap = new GameMap
 
-  def initialize() ={
-    for (i <- names) yield {
-
-    }
+  val numArmies = 50 - 5*names.size
+  var baseArmy: Set[Army] = Set()
+  for (i <- 1 to numArmies) {
+    baseArmy += new Infantry;
   }
-}
-
-object Game {
-  def newGame(names: Set[String]) = {
-    val numArmies = 50 - 5*names.size
-    var baseArmy: Set[Army] = Set()
-    for (i <- 1 to numArmies) {
-      baseArmy += new Infantry;
-    }
-    val players = for (i <- names) yield {
-      Player.addPlayer(i, baseArmy);
-    }
+  val players = for (i <- names) yield {
+    Player.addPlayer(i, baseArmy);
   }
+
+
 }
